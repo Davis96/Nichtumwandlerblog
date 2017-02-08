17cad40d928597a806087d31267d2cdd49669ae8
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-02-08 09:47:09 +0100

    Rewrite with template

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// enable all error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+$title = "DER Umwandlerblog";  // define variables for the template
+require("templates/index.html");  // load the template
+?>
